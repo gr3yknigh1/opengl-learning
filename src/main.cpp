c3317369f56bc6851bf0c3c08d2ca4bf08dbff21
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-03-28 09:45:33 +0300

    Fix error

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,39 @@
 #include <glm/glm.hpp>
 
 #define LOG(X) std::cout << X << '\n'
+#define STRINGIFY(X) (#X)
+#define ARRAY_COUNT(X) (sizeof((X)) / sizeof((X)[0]))
+
+/*
+ *
+ * Checkout all errors at <https://docs.gl/gl4/glGetError>
+ * */
+const char *GLEW_ErrorCodeDispatch(int errorCode)
+{
+    switch (errorCode)
+    {
+    case GL_NO_ERROR:
+        return STRINGIFY(GL_NO_ERROR);
+    case GL_INVALID_ENUM:
+        return STRINGIFY(GL_INVALID_ENUM);
+    case GL_INVALID_VALUE:
+        return STRINGIFY(GL_INVALID_VALUE);
+    case GL_INVALID_OPERATION:
+        return STRINGIFY(GL_INVALID_OPERATION);
+    case GL_INVALID_FRAMEBUFFER_OPERATION:
+        return STRINGIFY(GL_INVALID_FRAMEBUFFER_OPERATION);
+    case GL_OUT_OF_MEMORY:
+        return STRINGIFY(GL_OUT_OF_MEMORY);
+    case GL_STACK_UNDERFLOW:
+        return STRINGIFY(GL_STACK_UNDERFLOW);
+    case GL_STACK_OVERFLOW:
+        return STRINGIFY(GL_STACK_OVERFLOW);
+    default:
+        return "GL_UNHANDLED_ERROR";
+    }
+
+    return nullptr;
+}
 
 void GL_ClearErrors()
 {
@@ -18,17 +51,58 @@ void GL_CheckErrors(const char *glFunctionName, const char *sourceFilePath,
 {
     while (GLenum errorCode = glGetError())
     {
-        std::printf("[GL]: Error! Code %d\n", errorCode);
-        std::printf("[GL]: Error occur at %s::%d during %s call\n",
-                    sourceFilePath, sourceLine, glFunctionName);
+        std::printf("[GL]: %s - error code %d\n",
+                    GLEW_ErrorCodeDispatch(errorCode), errorCode);
+        std::printf("[GL]: Occured at %s::%d during %s call\n", sourceFilePath,
+                    sourceLine, glFunctionName);
         std::exit(EXIT_FAILURE);
     }
 }
 
-#define GL_Call(x)                                                             \
-    GL_ClearErrors();                                                          \
-    x;                                                                         \
-    GL_CheckErrors(#x, __FILE__, __LINE__)
+#define GL_Call(__glExpr)                                                      \
+    do                                                                         \
+    {                                                                          \
+        GL_ClearErrors();                                                      \
+        (__glExpr);                                                            \
+        GL_CheckErrors(#__glExpr, __FILE__, __LINE__);                         \
+    } while (0)
+
+#define GL_CallO(__glExpr, __outPtr)                                           \
+    do                                                                         \
+    {                                                                          \
+        GL_ClearErrors();                                                      \
+        *(__outPtr) = (__glExpr);                                              \
+        GL_CheckErrors(#__glExpr, __FILE__, __LINE__);                         \
+    } while (0)
+
+const char *GLFW_ErrorCodeDispatch(int errorCode)
+{
+    switch (errorCode)
+    {
+    case GLFW_NOT_INITIALIZED:
+        return STRINGIFY(GLFW_NOT_INITIALIZED);
+    case GLFW_NO_CURRENT_CONTEXT:
+        return STRINGIFY(GLFW_NO_CURRENT_CONTEXT);
+    case GLFW_INVALID_ENUM:
+        return STRINGIFY(GLFW_INVALID_ENUM);
+    case GLFW_OUT_OF_MEMORY:
+        return STRINGIFY(GLFW_OUT_OF_MEMORY);
+    case GLFW_API_UNAVAILABLE:
+        return STRINGIFY(GLFW_API_UNAVAILABLE);
+    case GLFW_VERSION_UNAVAILABLE:
+        return STRINGIFY(GLFW_VERSION_UNAVAILABLE);
+    case GLFW_PLATFORM_ERROR:
+        return STRINGIFY(GLFW_PLATFORM_ERROR);
+    case GLFW_FORMAT_UNAVAILABLE:
+        return STRINGIFY(GLFW_FORMAT_UNAVAILABLE);
+    case GLFW_NO_WINDOW_CONTEXT:
+        return STRINGIFY(GLFW_NO_WINDOW_CONTEXT);
+    default:
+        return "GLFW_UNHANDLED_ERROR";
+    }
+
+    return nullptr;
+}
 
 void GLFW_FrameBufferSizeCallback(GLFWwindow *window, int width, int height)
 {
@@ -37,7 +111,8 @@ void GLFW_FrameBufferSizeCallback(GLFWwindow *window, int width, int height)
 
 void GLFW_ErrorHandler(int errorCode, const char *description)
 {
-    std::printf("[GLFW]: Error! Code %d. Desc: %s\n", errorCode, description);
+    std::printf("[GLFW]: %s - error code %d, description: '%s'\n",
+                GLFW_ErrorCodeDispatch(errorCode), errorCode, description);
 }
 
 void GLFW_KeyCallback(GLFWwindow *window, int key, int scancode, int action,
@@ -49,10 +124,32 @@ void GLFW_KeyCallback(GLFWwindow *window, int key, int scancode, int action,
     }
 }
 
+void PrintDebugInfo()
+{
+    const unsigned char *glVersion = nullptr;
+    GL_CallO(glGetString(GL_VERSION), &glVersion);
+
+    const unsigned char *glGlslVersion = nullptr;
+    GL_CallO(glGetString(GL_SHADING_LANGUAGE_VERSION), &glGlslVersion);
+
+
+    const unsigned char *glRendererVersion = nullptr;
+    GL_CallO(glGetString(GL_RENDERER), &glRendererVersion);
+
+    const unsigned char *glVendor = nullptr;
+    GL_CallO(glGetString(GL_VENDOR), &glVendor);
+
+    std::printf("[GLFW]: %s\n", glfwGetVersionString());
+    std::printf("[GL]: OpenGL  : %s, GLSL: %s\n", glVersion, glGlslVersion);
+    std::printf("[GL]: Renderer: %s\n", glRendererVersion);
+    std::printf("[GL]: Vendor  : %s\n", glVendor);
+}
+
 int main(void)
 {
     if (!glfwInit())
     {
+        std::printf("[GLFW]: Error! Failed initialize glfw\n");
         return EXIT_FAILURE;
     }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -61,32 +158,58 @@ int main(void)
     // NOTE: For compatibility
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    if (!glewInit())
-    {
-        return EXIT_FAILURE;
-    }
-
-    std::printf("[GLFW]: %s\n", glfwGetVersionString());
-
     glm::vec2 windowSize = {900, 600};
     const char *title = "Triangle";
     GLFWwindow *window =
-        glfwCreateWindow(windowSize.x, windowSize.y, title, 0, 0);
-
+        glfwCreateWindow(windowSize.x, windowSize.y, title, nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
+        std::printf("[GLFW]: Error! Failed create window\n");
         return EXIT_FAILURE;
     }
+
     glfwMakeContextCurrent(window);
     glfwSetErrorCallback(GLFW_ErrorHandler);
     glfwSetKeyCallback(window, GLFW_KeyCallback);
     glfwSetFramebufferSizeCallback(window, GLFW_FrameBufferSizeCallback);
+    glfwSwapInterval(true);
+
+    if (glewInit() != GLEW_OK)
+    {
+        glfwTerminate();
+        std::printf("[GLEW]: Error! Failed initialize glew\n");
+        return EXIT_FAILURE;
+    }
+
+    PrintDebugInfo();
 
     GL_Call(glViewport(0, 0, windowSize.x, windowSize.y));
+
     // ------------------------------------------------------ //
 
     glm::vec4 clearColor = {.1f, .1f, .1f, 1.f};
+    float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
+                        0.0f,  0.0f,  0.5f, 0.0f};
+
+    uint32_t vbo = 0;
+    GL_Call(glGenBuffers(1, &vbo));
+    GL_Call(glBindBuffer(GL_ARRAY_BUFFER, vbo));
+    GL_Call(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+                         GL_STATIC_DRAW));
+
+    const char *vertexShaderSource =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    uint32_t vertexShader = 0;
+    GL_CallO(glCreateShader(GL_VERTEX_SHADER), &vertexShader);
+    GL_Call(glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr));
+    GL_Call(glCompileShader(vertexShader));
 
     while (!glfwWindowShouldClose(window))
     {
