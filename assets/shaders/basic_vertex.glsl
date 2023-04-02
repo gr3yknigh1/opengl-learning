9fad3981b6507d3b3ea305d8b8d3d17e2e7e9e64
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-04-02 09:19:19 +0300

    Update shaders

--- assets/shaders/basic_vertex.glsl
+++ assets/shaders/basic_vertex.glsl
@@ -2,8 +2,12 @@
 
 layout (location = 0) in vec3 aPos;
 
+out vec4 vertexColor;
+
 void main()
 {
-   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    vertexColor = gl_Position + vec4(0.3, 0.3, 0.3, 1);
+    vertexColor.a = 0.9;
 };
 
