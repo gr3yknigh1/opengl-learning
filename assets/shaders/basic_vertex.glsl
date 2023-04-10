13f027357f63abbb15e6a394ed0e502c5707faad
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-04-10 16:06:22 +0300

    Add `u_Transform` uniform to basic_vertex shader

--- assets/shaders/basic_vertex.glsl
+++ assets/shaders/basic_vertex.glsl
@@ -7,12 +7,14 @@ layout (location = 2) in vec2 l_TexCoord;
 out vec4 f_Color;
 out vec2 f_TexCoord;
 
+uniform mat4 u_Transform;
 uniform float u_VertexModifier = 1;
 uniform vec3 u_VertexOffset = vec3(0, 0, 0);
 
 void main()
 {
-    gl_Position = vec4(l_Position * u_VertexModifier + u_VertexOffset, 1.0);
+    gl_Position = u_Transform * vec4(l_Position * u_VertexModifier
+                                   + u_VertexOffset, 1.0);
 
     f_Color = vec4(l_Color, 1);
     f_TexCoord = l_TexCoord;
