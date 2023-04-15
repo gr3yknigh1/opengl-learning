1c80e16b5cd989ce5a170c01ae5a460d1986121d
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-04-15 18:39:35 +0300

    Remove imgui macro

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#define IMGUI_IMPL_OPENGL_LOADER_GLFW
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
