8eb434b40df5fde6e2b060963d401a77d7a9c0de
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-04-16 16:55:40 +0300

    Add `Function` from testing project

---
+++ include/glsandbox/Core/Function.hpp
@@ -0,0 +1,43 @@
+#ifndef FUNCTION_HPP_
+#define FUNCTION_HPP_
+
+template <typename ReturnType, typename... Args> class Function {
+  using FunctionPointerType = ReturnType (*)(Args...);
+
+public:
+  Function() = delete;
+  Function(FunctionPointerType functionPointer)
+      : m_FunctionPointer(functionPointer) {}
+  Function(Function &function)
+      : m_FunctionPointer(function.m_FunctionPointer) {}
+  Function(Function &&function) noexcept
+      : m_FunctionPointer(function.m_FunctionPointer) {}
+
+  virtual ~Function() {}
+
+  inline void Set(FunctionPointerType functionPointer) {
+    m_FunctionPointer = functionPointer;
+  }
+
+  inline ReturnType Call(Args... args) const {
+    return m_FunctionPointer(args...);
+  }
+
+  ReturnType operator()(Args... args) const { Call(args...); }
+
+  Function &operator=(const Function &function) { Set(function); }
+
+  Function &operator=(Function &&function) noexcept { Set(function); }
+
+  Function &operator=(const FunctionPointerType &functionPointer) {
+    Set(functionPointer);
+  }
+  Function &operator=(FunctionPointerType &&functionPointer) noexcept {
+    Set(functionPointer);
+  }
+
+private:
+  FunctionPointerType m_FunctionPointer;
+};
+
+#endif // FUNCTION_HPP_
